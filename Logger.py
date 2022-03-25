0bd6e57c42c8bad07e4b9f739841393f3dca167f
WarBringerLT <79408905+WarBringerLT@users.noreply.github.com>
2022-03-25 20:41:30 +0000

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -5,32 +5,28 @@
 #                                                             #
 ###############################################################
 
-### [All Other Dependent Modules]
+### [All Dependent Modules]
 from os import path, chdir, mkdir
 from sys import argv
 from time import ctime, time
 from datetime import datetime
-### [All Other Dependent Modules] END
+#################################
 
 ### [SCRIPT INIT]
 script_init_time = time() # Calculate how long it took for the module to load later
 chdir(path.dirname(argv[0])) # CHANGE THE SCRIPT WORKING  DIRECTORY TO .py File Location
 
-## [CODE LEVELS]
+## [ERROR CODE LEVELS]
 Error_Codes = [ "INFO",    # ID 0
-		"WARNING", # ID 1
-		"ERROR",   # ID 2
-		"CRITICAL" # ID 3
-		# Continue etc...
-		]
-
-## [CODE LEVELS] END
-### [SCRIPT INIT] END
-
+				"WARNING", # ID 1
+				"ERROR",   # ID 2
+				"CRITICAL" # ID 3
+				] # Continue etc...
+				
 ## [MAIN PRINT AND SAVE MODULE]
 class Logging:
 	def __init__(self):
-		self.Timestamp = ""
+		self.Timestamp = "" # ONLY DECLARATION OF VALUE - DO NOT EDIT - DYNAMIC CACHE VARIABLE
 		self.Todays_Date = datetime.today().strftime('%d-%m-%Y')
 		self.Log_Folder = "Logs/"
 		self.Log_File   = self.Log_Folder + self.Todays_Date + '.ini' # - Log file will be DD-MM-YYYY.ini Files 
@@ -79,8 +75,6 @@ class Logging:
 		print(FinalLog)
 		return True
 
-## [MAIN PRINT AND SAVE MODULE] END
-
 ## %% ON START %% ##
 Logging = Logging() # INITIALIZE Logging System
 # Print Succesfull Start
@@ -93,16 +87,3 @@ if __name__ == "__main__": # IF SCRIPT IS LAUNCHED DIRECTLY
 	for item in Error_Codes:
 		Code_Number = int(Error_Codes.index(item))
 		Logging.log(Code_Number,f"Testing Error Code: {item} (CODE: {Code_Number})")
-
-
-##### README:
-#
-# TO USE IN YOUR OWN PYTHON SCRIPT (TO YOUR OTHER FILE ADD)
-#
-# from Logger.py import *
-#
-# Usage:
-# Logging.log(CODE_NUMBER, MESSAGE) 
-# 
-# It will automatically create log folder, create log file, and insert all messages
-# that were "announced" in the app
