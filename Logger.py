e8b5bcce0b83a7e74b98c397ba9b798a29725995
WarBringerLT <79408905+WarBringerLT@users.noreply.github.com>
2022-03-26 10:14:14 +0000

    Add files via upload

--- Logger.py
+++ Logger.py
@@ -18,24 +18,27 @@ chdir(path.dirname(argv[0])) # CHANGE THE SCRIPT WORKING  DIRECTORY TO .py File 
 
 ## [ERROR CODE LEVELS]
 Error_Codes = [ "INFO",    # ID 0
-		"WARNING", # ID 1
-		"ERROR",   # ID 2
-		"CRITICAL" # ID 3
-		] # Continue etc...
+				"WARNING", # ID 1
+				"ERROR",   # ID 2
+				"CRITICAL" # ID 3
+				] # Continue etc...
 
 ## [MAIN PRINT AND SAVE MODULE]
 class Logging:
+	Timestamp = "" # ONLY DECLARATION OF VALUE - DO NOT EDIT - DYNAMIC CACHE VARIABLE
 	Todays_Date = datetime.today().strftime('%d-%m-%Y')
 	Log_Folder = "Logs/"
 	Log_File   = Log_Folder + Todays_Date + '.ini' # - Log file will be DD-MM-YYYY.ini Files 
 	Verbose_Output = True # True/False - Whether show output from Logging Module
 	
 	def __init__(self):
+		global Timestamp,Todays_Date,Log_Folder,Log_File,Verbose_Output
 		
 		# SELECT Timestamp_Setting:    "LOCALTIME" (LOCALTIME OF PC)  [DEFAULT PRINT: HH:MM:SS]
 		# OR SELECT Timestamp_Setting: "RUNTIME" (RUNTIME of the app) [DEFAULT PRINT: [0.0s] ]
 		self.Timestamp_Setting = "LOCALTIME"
-		if self.Timestamp_Setting == "LOCALTIME": self.Timestamp = datetime.now().strftime('[%H:%M:%S] ')
+		
+		if self.Timestamp_Setting == "LOCALTIME": self.Timestamp = datetime.now().strftime('[%H:%M:%S]>')
 		elif self.Timestamp_Setting == "RUNTIME": self.Timestamp = f"[{round(time()-script_init_time,3)}s]>"
 			
 		# Code below will check whether LOGS FOLDER exists, if not, generate one
@@ -52,7 +55,7 @@ class Logging:
 			logfile.write(f"#> Log File was first generated at {ctime()}.")
 			logfile.close()
 
-	def log(self, code, message, ifprint = True):
+	def log(self, message, code=0, ifprint = True):
 		# self    = self.
 		# code    = Error Code 
 		# Message = Contents of Log Message
@@ -60,18 +63,20 @@ class Logging:
 		#however, messages can be suppresed (to log only) with no output/silent
 		#if specified with False parameter
 
-		FinalLog = f"{self.Timestamp} [{Error_Codes[int(code)]}]: {message}"
+		FinalLog = f"{Logging.Timestamp} [{Error_Codes[int(code)]}] - {message}"
 		f = open(Logging.Log_File,'a')
 		f.write('\n'+FinalLog)
 		f.close()
 		if Logging.Verbose_Output or ifprint:
-			try: Logging.print(self, code, message)
-			except TypeError: Logging.print(code, message)
+			try:
+				Logging.print(self, message, code)
+			except TypeError:
+				Logging.print(message, code)
 		return True
 
-	def print(self, code, message):
+	def print(self, message, code = 0):
 		# Output of the Log After Storing
-		FinalLog = f"{self.Timestamp} [{Error_Codes[int(code)]}]: {message}"
+		FinalLog = f"{Logging.Timestamp} [{Error_Codes[int(code)]}] - {message}"
 		print(FinalLog)
 		return True
 
@@ -79,10 +84,10 @@ class Logging:
 if __name__ == "__main__": # IF SCRIPT IS LAUNCHED DIRECTLY
 	Logging = Logging() # INITIALIZE Logging System
 	# Print Succesfull Start
-	Logging.log(0,f"Logging Started - Init Successful - Time Taken: {time()-script_init_time}s")
+	Logging.log(f"Logging Started - Init Successful - Time Taken: {time()-script_init_time}s")
 	print(f"Log File: {Logging.Log_File}")
 	print(f"File Directory Created: {path.isfile(Logging.Log_File)}")
 	print(f"Found Total: {len(Error_Codes)} Error_Codes Codes. Cycling Through All of them")
 	for item in Error_Codes:
 		Code_Number = int(Error_Codes.index(item))
-		Logging.log(Code_Number,f"Testing Error Code: {item} (CODE: {Code_Number})")
+		Logging.log(f"Testing Error Code: {item} (CODE: {Code_Number})", Code_Number)
--- Logger_Stripped.py
+++ Logger_Stripped.py
@@ -16,17 +16,15 @@ Error_Codes = [ "INFO",    # ID 0
 				"CRITICAL" # ID 3
 				]
 class Logging:
-	Timestamp = ""
 	Timestamp_Setting = "LOCALTIME"
 	Todays_Date = datetime.today().strftime('%d-%m-%Y')
 	Log_Folder = "Logs/"
 	Log_File   = Log_Folder + Todays_Date + '.ini' # - Log file will be DD-MM-YYYY.ini Files 
 	Verbose_Output = True # True/False - Whether show output from Logging Module
 	def __init__(self):
-		global Timestamp
 		self.Timestamp_Setting = Logging.Timestamp_Setting
-		if self.Timestamp_Setting == "LOCALTIME": Timestamp = datetime.now().strftime('[%H:%M:%S] ')
-		elif self.Timestamp_Setting == "RUNTIME": Timestamp = f"[{round(time()-script_init_time,3)}s]>"
+		if self.Timestamp_Setting == "LOCALTIME": self.Timestamp = datetime.now().strftime('[%H:%M:%S] ')
+		elif self.Timestamp_Setting == "RUNTIME": self.Timestamp = f"[{round(time()-script_init_time,3)}s]>"
 		if not path.isdir(self.Log_Folder):
 			if self.Verbose_Output: print(f"{self.Timestamp} Logs Folder was not found - attempting to create one...")
 			mkdir(self.Log_Folder)
@@ -35,28 +33,27 @@ class Logging:
 			logfile = open(self.Log_File,'w')
 			logfile.write(f"#> Log File was first generated at {ctime()}.")
 			logfile.close()
-	def log(self, code, message, ifprint = True):
-		FinalLog = f"{Timestamp} [{Error_Codes[int(code)]}]: {message}"
+
+	def log(self, message, code = 0, ifprint = True):
+		FinalLog = f"{Logging.Timestamp} [{Error_Codes[int(code)]}]: {message}"
 		f = open(Logging.Log_File,'a')
 		f.write('\n'+FinalLog)
 		f.close()
 		if Logging.Verbose_Output or ifprint:
-			try: Logging.print(self, code, message)
-			except TypeError: Logging.print(code, message)
+			try: Logging.print(self, message, code)
+			except TypeError: Logging.print(message, code)
 		return True
-	def print(self, code, message):
+	def print(self, message, code = 0):
 		# Output of the Log After Storing
-		if self.Timestamp_Setting == "LOCALTIME": Timestamp = datetime.now().strftime('[%H:%M:%S] ')
-		elif self.Timestamp_Setting == "RUNTIME": Timestamp = f"[{round(time()-script_init_time,3)}s]>"
-		FinalLog = f"{Timestamp} [{Error_Codes[int(code)]}]: {message}"
+		FinalLog = f"{Logging.Timestamp} [{Error_Codes[int(code)]}]: {message}"
 		print(FinalLog)
 		return True
 if __name__ == "__main__": # IF USED IN ANOTHER SCRIPT ALL LINES BELOW (INCLUDING THIS LINE) CAN BE DELETED TO USE EVEN LESS SPACE
 	Logging = Logging() 
-	Logging.log(0,f"Logging Started - Init Successful - Time Taken: {time()-script_init_time}s")
+	Logging.log(f"Logging Started - Init Successful - Time Taken: {time()-script_init_time}s",0)
 	print(f"Log File: {Logging.Log_File}")
 	print(f"File Directory Created: {path.isfile(Logging.Log_File)}")
 	print(f"Found Total: {len(Error_Codes)} Error_Codes Codes. Cycling Through All of them")
 	for item in Error_Codes:
 		Code_Number = int(Error_Codes.index(item))
-		Logging.log(Code_Number,f"Testing Error Code: {item} (CODE: {Code_Number})")
+		Logging.log(f"Testing Error Code: {item} (CODE: {Code_Number})",Code_Number)
